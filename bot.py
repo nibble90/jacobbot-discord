e5ba63ae9152da36683ffe5b5c44604e84458c1d
nibble90 <18497547+nibble90@users.noreply.github.com>
2020-11-10 15:04:08 +0000

    As before, changed to relative

--- bot.py
+++ bot.py
@@ -3,53 +3,52 @@ import random
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
-from sys import path
-path.append("../reddit-grabber")
-from main_classes import RedditAPI
-
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+from redditgrabber.main_classes import RedditAPI
 
 bot = commands.Bot(command_prefix="+")
 
-@bot.event
-async def on_ready():
-    print("{} has connected to Discord".format(bot.user.name))
-#    for guild in client.guilds:
-#        print("Connected to guild {}, id:{}".format(guild.name, guild.id))
-    guild = discord.utils.get(bot.guilds)
-#    print(repr(guild))
-    print("Connected to guild {}, id:{}".format(guild.name, guild.id))
+class DiscordBot:
+    def __init__(self):
+        load_dotenv()
+        self.TOKEN = os.getenv('DISCORD_TOKEN')
+        # bot = commands.Bot(command_prefix="+")
+        bot.run(self.TOKEN)
 
-@bot.command(name="hello", help="Responds with a hello message")
-async def hello(ctx):
-    hellos = [
-        "Hello! <@{}>",
-        "Hey! <@{}>",
-        "Grandma <@{}>, is that you?",
-        "<@{}>! you finally came back, where is the milk though?",
-        "<@{}>, who even are you?"]
+    @staticmethod
+    @bot.event
+    async def on_ready():
+        print("{} has connected to Discord".format(bot.user.name))
+    #    for guild in client.guilds:
+    #        print("Connected to guild {}, id:{}".format(guild.name, guild.id))
+        guild = discord.utils.get(bot.guilds)
+    #    print(repr(guild))
+        print("Connected to guild {}, id:{}".format(guild.name, guild.id))
 
-    response = random.choice(hellos)
-    response = response.format(ctx.author.id)
-    await ctx.send(response)
+    @staticmethod
+    @bot.command(name="hello", help="Responds with a hello message")
+    async def hello(ctx):
+        hellos = [
+            "Hello! <@{}>",
+            "Hey! <@{}>",
+            "Grandma <@{}>, is that you?",
+            "<@{}>! you finally came back, where's the milk though?",
+            "<@{}>, who even are you?"]
 
-@bot.command(name="pic", help="Responds with the current top image from r/pics")
-async def reddit_pic(ctx):
-    pics = RedditAPI().pics()
-    for (title, score, url, selftext, author, post_id) in pics:
-        attach = discord.Embed(
-            title=title,
-            description=selftext,
-            url="https://redd.it/"+str(post_id)
-        )
-        attach.set_image(url=url)
-        attach.add_field(name="Upvotes", value=score)
-        attach.add_field(name="User", value="u/" + str(author))
-        await ctx.send(embed=attach)
+        response = random.choice(hellos)
+        response = response.format(ctx.author.id)
+        await ctx.send(response)
 
-
-
-
-
-bot.run(TOKEN)
+    @staticmethod
+    @bot.command(name="pic", help="Responds with the current top image from r/pics")
+    async def reddit_pic(ctx):
+        pics = RedditAPI().pics()
+        for (title, score, url, selftext, author, post_id) in pics:
+            attach = discord.Embed(
+                title=title,
+                description=selftext,
+                url="https://redd.it/"+str(post_id)
+            )
+            attach.set_image(url=url)
+            attach.add_field(name="Upvotes", value=score)
+            attach.add_field(name="User", value="u/" + str(author))
+            await ctx.send(embed=attach)
